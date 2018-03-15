c217d8530e6d166daf76d0f9514e468973511ce2
Cristian Nicolás Berrogorry Llano <nicoberrogorry@gmail.com>
2018-03-15 04:31:54 -0300

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -5,65 +5,58 @@
 import collections
 
 if __name__ == "__main__":
-    limit = 4000000
+    # Brute force approach
+    prevFib = 1
+    # 1 is NOT even -> efsum = 0
 
-    # Brute force
-    prev_fib = 1
     fib = 2
+    # 2 is even -> efsum += 2
+    evenFibSum += fib
+
+    # Calculate, compare and sum the next terms before four million
+    while fib < 4000000:
+        if fib % 2 == 0:
+            evenFibSum += next_fib
+        backupFib = fib
+        fib += prevFib
+        prevFib = fib
+
+    print("Brute: {}".format(evenFibSum))
+
+
+    # More efficient approach
+    '''
+        Introduction:
+            The sum of an odd number and an even number is odd.
+        Its trivial to prove the sum of two odd numbers is an even number.
+        
+        f(1) = 1 -> odd
+        f(2) = 1 -> odd
+        f(3) = 1 + 1 = 2 -> even
+        f(4) = 2 + 3 = 5 -> odd
+        f(5) = 5 + 2 = 7 -> odd
+        f(6) = 5 + 7 = 12 ->even
+
+        Looks like every third term is even...
+        
+        Fibonacci's sequence closed form:
+        f(n) = (pow(1 + sqrt(5), n) - pow(1 - sqrt(5), n)) / sqrt(5)
+        
+        So every third term's closed form is:
+        f(3n) = (pow(1 + sqrt(5), 3n) - pow(1 - sqrt(5), 3n)) / sqrt(5)
 
     '''
-        Later we'll always test against the next Fibonacci terms before the limit, so we test and
-        sum the first two terms manually:
-            1 is NOT even -> s = 0
-            2 IS even -> s += 2 -> s = 2
-    '''
-    s = 2
+    sqrt5 = sqrt(5)
+    gr = 1 + sqrt5
+    ngr = 1 - sqrt5
+    
+    evenFibSum = 0
+    k = 3
+    fib = 2
+    while fib < 4000000:
+        fib = (pow(gr, k) - pow(ngr, k))
+        evenFibSum = evenFibSum + fib
+        k = k + 3
+    evenFibSum = evenFibSum / sqrt5
 
-    # Work on the next pair of Fibonacci terms
-    go_on = True
-    while go_on:
-        prev_fib += fib
-        # Test limit
-        if prev_fib < limit:
-            # Test and sum
-            if prev_fib % 2 == 0:
-                s += prev_fib
-
-            fib += prev_fib
-            if fib % 2 == 0:
-                s += fib
-        else:
-            # Limit hit
-            go_on = False
-
-    print("Brute force says: {}".format(s))
-
-    # Yet another solution, by Project Euler
-    '''
-        Every third Fibonacci number can be calculated with a recursive function:
-
-            g(n) = 4 * g(n-1) + g(n-2)
-
-        Note: This proof can be obtained by proving f(n) = 4 * f (n-3) + f(n-6) for every nth
-              Fibonacci term.
-    '''
-    def g(n, res=collections.Counter({'sum': 10, 'calls': 0}), gn__: int = None, gn_: int = None):
-        res['calls'] += 1
-
-        if gn__ is None or gn_ is None:
-            if n == 3:
-                gn__, gn_ = 2, 8
-            else:
-                _, gn__, gn_ = g(n - 1)
-
-        gn = 4 * gn_ + gn__
-
-        if gn < limit:
-            res['sum'] += gn
-
-        # The parent g(n) recycles the value g(n-1) and g(n), avoiding
-        return res, gn_, gn
-
-    g_res, _, _ = g(11)
-
-    print("Recursive solution: {} with {} recursions".format(g_res['sum'], g_res['calls']))
+    print("Efficient: {}".format())
